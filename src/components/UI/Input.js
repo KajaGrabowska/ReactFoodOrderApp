292ea9ad41947c46fb598e023a54a6c4b25b78e9
Kaja Grabowska <kaja.d.grabowska@gmail.com>
2021-06-29 21:08:00 -0500

    Added Add food to order form and input UI component

---
+++ src/components/UI/Input.js
@@ -0,0 +1,15 @@
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
+
+// {...props.input} spread operator, this ensures that all key value pairs in 
+//this input object are added as props to the input element
